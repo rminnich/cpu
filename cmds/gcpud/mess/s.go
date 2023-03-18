a43cdc474fe6e8195b207532e1a2532a7d159e39
Ronald G. Minnich <rminnich@gmail.com>
2023-03-17 19:05:02 -0700

    messing around. Don't push this.
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ cmds/gcpud/mess/s.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os/exec"
+	"sync"
+
+	pb "github.com/pramonow/go-grpc-server-streaming-example/src/proto"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (s server) FetchResponse(in *pb.Request, srv pb.StreamService_FetchResponseServer) error {
+
+	log.Printf("fetch response for id : %d", in.Id)
+
+	c := exec.Command("bash")
+	var err error
+	stdin, err := c.StdinPipe()
+	if err != nil {
+		return err
+	}
+	stdout, err := c.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	stderr, err := c.StderrPipe()
+	if err != nil {
+		return err
+	}
+
+	var wg sync.WaitGroup
+	{
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			for {
+				var b [1]byte
+				if _, err := stdout.Read(b[:]); err != nil {
+					log.Printf("ou error %v", err)
+					return
+				}
+				resp := pb.Response{Result: fmt.Sprintf("%c %v", b[0], err)}
+				if err := srv.Send(&resp); err != nil {
+					log.Printf("send error %v", err)
+					return
+				}
+			}
+		}()
+		go func() {
+			defer wg.Done()
+			for {
+				var b [1]byte
+				if _, err := stderr.Read(b[:]); err != nil {
+					log.Printf("ou error %v", err)
+					return
+				}
+				resp := pb.Response{Result: fmt.Sprintf("%c %v", b[0], err)}
+				if err := srv.Send(&resp); err != nil {
+					log.Printf("send error %v", err)
+					return
+				}
+			}
+		}()
+	}
+	if _, err := stdin.Write([]byte("date\n")); err != nil {
+		log.Printf("write command: %v", err)
+	}
+	if err := c.Run(); err != nil {
+		log.Printf("run: %v", err)
+	}
+	wg.Wait()
+	return nil
+}
+
+func main() {
+	// create listiner
+	lis, err := net.Listen("tcp", ":50005")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	// create grpc server
+	s := grpc.NewServer()
+	pb.RegisterStreamServiceServer(s, server{})
+
+	log.Println("start server")
+	// and start...
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
