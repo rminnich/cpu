54f0a6d56b0aab1a2b7ecc562fadaa044e9aefd6
Ronald G. Minnich <rminnich@gmail.com>
2023-03-21 14:09:27 -0700

    first pass, starting a shell
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ cmds/gcpud/helloworld/greeter_client/main.go
@@ -0,0 +1,115 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Package main implements a client for Greeter service.
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os/exec"
+	"sync"
+	"time"
+
+	pb "github.com/u-root/cpu/cmds/gcpud/helloworld/helloworld"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	defaultName = "world"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	name = flag.String("name", defaultName, "Name to greet")
+)
+
+func main() {
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	cmd := exec.Command("bash")
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return
+	}
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return
+	}
+
+	var wg sync.WaitGroup
+	{
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			for {
+				var b [1]byte
+				if _, err := stdout.Read(b[:]); err != nil {
+					log.Printf("ou error %v", err)
+					return
+				}
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+				defer cancel()
+				r, err := c.SayHello(ctx, &pb.HelloRequest{Name: string(b[:])})
+				if err != nil {
+					log.Printf("could not greet: %v, %v", r, err)
+					return
+				}
+				log.Printf("Greeting: %s", r.GetMessage())
+			}
+		}()
+		go func() {
+			defer wg.Done()
+			for {
+				var b [1]byte
+				if _, err := stderr.Read(b[:]); err != nil {
+					log.Printf("ou error %v", err)
+					return
+				}
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+				defer cancel()
+				r, err := c.SayHello(ctx, &pb.HelloRequest{Name: string(b[:])})
+				if err != nil {
+					log.Printf("could not greet: %v, %v", r, err)
+					return
+				}
+			}
+		}()
+	}
+
+	if _, err := stdin.Write([]byte("date\n")); err != nil {
+		log.Printf("write command: %v", err)
+	}
+	if err := cmd.Run(); err != nil {
+		log.Printf("run: %v", err)
+	}
+}
